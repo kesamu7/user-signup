cf68a5757eec694c5cb8c067c3d139342c22c33a
Kelvin Quinonez <kesqui7@gmail.com>
2017-02-02 19:42:09 -0600

    Completed user-signup assignment.

--- main.py
+++ main.py
@@ -15,11 +15,37 @@
 # limitations under the License.
 #
 import webapp2
+import re
+import cgi
+
+
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return  not email or EMAIL_RE.match(email)
+
+
+
 
 page_header = """
 <!DOCTYPE html>
 <html>
 <head>
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
     <title>User Signup</title>
 </head>
     <h1>User Signup</h1>
@@ -32,43 +58,113 @@ page_footer = """
 """
 
 
+def main_page(emailerror='',
+               verification='',
+               invalidusernm='',
+               invpassword='',
+               username='',
+               email=''):
+    user_info = """
+    <form action="/" method="post">
+      <label>Username
+          <input type="text" name="username" value={username}>
+      </label>
+      <span class="error">{invalidusernm}</span>
+      <br>
+
+      <label>Password
+        <input type = "password" name="password" />
+      </label>
+      <span class="error">{invpassword}</span>
+      <br>
+
+      <label>Verify Password
+        <input type = "password" name="verify" />
+      </label>
+      <span class="error">{verification}</span>
+      <br>
+
+      <label>Email(optional)
+        <input type = "text" name="email" value={email}>
+      </label>
+      <span class="error">{emailerror}</span>
+      <br>
+      <input type="submit" value="Sign Up"/>
+
+    </form>
+
+
+    """.format(emailerror = emailerror,
+               verification = verification,
+               invalidusernm = invalidusernm,
+               invpassword = invpassword,
+               username=username,
+               email=email)
+
+    return user_info
+
 
 
 class Index(webapp2.RequestHandler):
-
     def get(self):
-        user_name = """
-        <form action="/" method="post">
-          <label>Username
-              <input type="text" name="username"/>
-          </label>
-          <br>
-          <label>Password
-            <input type = "password" name="password"/>
-          </label>
-          <br>
-          <label>Verify Password
-            <input type = "password" name="verify-password"/>
-          </label>
-          <br>
-          <label>Email(optional)
-            <input type = "text" name="email"/>
-          </label>
-          <input type="submit" value="Sign Up"/>
-        </form>
-        """
+        self.response.write(page_header + main_page() + page_footer)
 
-        self.response.write(page_header + user_name + page_footer)
 
 
     def post(self):
-        username = self.request.get('username')
-        self.redirect('/welcome?username={}'.format(username))
+        success = True
+        username = cgi.escape(self.request.get('username'))
+        password = cgi.escape(self.request.get("password"))
+        verify = cgi.escape(self.request.get("verify"))
+        email = cgi.escape(self.request.get("email"))
+
+
+
+        if valid_email(email):
+            emailerror = ''
+        else:
+            emailerror = 'Invalid Email'
+            success = False
+
+        if valid_username(username):
+            usn_error = ''
+        else:
+            usn_error = 'Invalid username.'
+            success = False
+
+        if valid_password(password):
+            pwerror = ''
+        else:
+            pwerror = 'Invalid password'
+            success = False
+
+        if password != verify:
+            ver_error="Passwords do not match."
+            success = False
+        else:
+            ver_error = ''
+
+
+        if success:
+
+            self.redirect('/welcome?username={}'.format(username))
+        else:
+            self.response.write(page_header + main_page(
+                emailerror=emailerror,
+               verification=ver_error,
+               invalidusernm=usn_error,
+               invpassword=pwerror,
+               username = username,
+               email = email) + page_footer )
+
+
+
 
 class Welcome(webapp2.RequestHandler):
     def get(self):
-        the_user = self.request.get("username")
-        self.response.write("Welcome, " + the_user + "!")
+        username = self.request.get("username")
+
+        self.response.write("Welcome, " + username + "!")
 
 
 
